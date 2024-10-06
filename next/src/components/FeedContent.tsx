6e178970b809b9bde16f1110b8f0955e7731e29f
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-10-06 11:20:40 +0200

    ADD(post): styling to interactive buttons

--- next/src/components/FeedContent.tsx
+++ next/src/components/FeedContent.tsx
@@ -6,27 +6,29 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { MessageCircle, Repeat2, Heart, Bookmark, Share } from 'lucide-react';
 import Link from "next/link";
+import { useUserContext } from "@/context/UserContextProvider";
 
 export default function FeedContent() {
-    const [feedPosts, setFeedPosts] = useState <PostInfoType[] | undefined | null>(undefined);
+    const [feedPosts, setFeedPosts] = useState<PostInfoType[] | undefined | null>(undefined);
+    const { user } = useUserContext();
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await fetch('/api/feedPosts', {
+                const response = await fetch('/api/posts/feed/global', {
                     method: 'GET',
                 });
 
                 if (!response.ok) {
                     const errorData = await response.json();
                     console.log(errorData);
-                    
+
                     throw new Error(errorData.error);
                 }
 
                 const feedData: PostInfoType[] = await response.json();
                 console.log(feedData);
-                
+
                 setFeedPosts(feedData);
             } catch (error) {
 
@@ -35,6 +37,57 @@ export default function FeedContent() {
 
         fetchPosts();
     }, []);
+
+    const handlePostBtnsInteraction = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, postId: number) => {
+        const btn = e.currentTarget;
+        const type = btn.dataset.type as string;
+        const styleType = type === 'like' ? 'lik' : type;
+        const status = btn.dataset.status;
+
+        btn.disabled = true;
+        // optimistically change the styling
+        btn.classList.toggle(`${styleType}ed`);
+        if (status === 'true') {
+            try {
+                const response = await fetch(`/api/posts/remove${type.charAt(0).toUpperCase() + type.slice(1)}/${postId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Failed to remove the ${type} on the post`);
+                }
+
+                btn.dataset.status = 'false';
+            } catch (error) {
+                console.log(error);
+                // revert the styling
+                btn.classList.toggle(`${styleType}ed`);
+            }
+        } else {
+            try {
+                const response = await fetch(`/api/posts/${type}/${postId}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Failed to ${type} the post`);
+                }
+
+                btn.dataset.status = 'true';
+            } catch (error) {
+                console.log(error);
+                // revert the styling
+                btn.classList.toggle(`${styleType}ed`);
+            }
+        }
+        btn.disabled = false;
+    };
 
     if (feedPosts === undefined) return <div>loading...</div>;
     if (feedPosts === null) return <div>No posts</div>;
@@ -68,22 +121,37 @@ export default function FeedContent() {
                                             </div>
                                             <p>{post.replies.length}</p>
                                         </Link>
-                                        <button className='repost-btn group'>
+
+                                        <button
+                                            className={`repost-btn group ${post.reposts.some((repost) => repost.userId === user.id) ? 'reposted' : ''}`}
+                                            data-type='repost'
+                                            data-status={`${post.reposts.some((repost) => repost.userId === user.id)}`}
+                                            onClick={(e) => handlePostBtnsInteraction(e, post.id)}>
                                             <span className='h-[35px] w-[35px] rounded-full flex-center group-hover:bg-green-500/10'>
                                                 <Repeat2 size={24} className='text-dark-400 group-hover:text-green-500/70' />
                                             </span>
                                             <p>{post.reposts.length}</p>
                                         </button>
-                                        <button className='like-btn group'>
+
+                                        <button
+                                            className={`like-btn group ${post.likes.some((like) => like.userId === user.id) ? 'liked' : ''}`}
+                                            data-type='like'
+                                            data-status={`${post.likes.some((like) => like.userId === user.id)}`}
+                                            onClick={(e) => handlePostBtnsInteraction(e, post.id)}>
                                             <span className='h-[35px] w-[35px] rounded-full flex-center group-hover:bg-pink-500/10'>
-                                                <Heart size={20} className='text-dark-400 group-hover:text-pink-500/70' />
+                                                <Heart size={20} className='text-dark-400 group-hover:text-pink-500' />
                                             </span>
                                             <p>{post.likes.length}</p>
                                         </button>
                                     </div>
-                                    <button className='bookmark-btn group'>
-                                        <Bookmark size={20} className='text-dark-400 group-hover:text-blue-1/70' />
+                                    <button
+                                        className={`bookmark-btn group ${post.bookmarks.some((bookmark) => bookmark.userId === user.id ? 'bookmarked' : '')}`}
+                                        data-type='bookmark'
+                                        data-status={`${post.bookmarks.some((bookmark) => bookmark.userId === user.id)}`}
+                                        onClick={(e) => handlePostBtnsInteraction(e, post.id)}>
+                                        <Bookmark size={20} className='text-dark-400 text-blue-1/70' />
                                     </button>
+
                                     <button className='share-btn group'>
                                         <Share size={20} className='text-dark-400 group-hover:text-blue-1/70' />
                                     </button>
