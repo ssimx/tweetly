f23e625e910d165093437c319dc620d2d4a57208
vespucchi <56511717+vespucchi@users.noreply.github.com>
2025-03-03 13:33:52 +0100

    fix(trending): hashtags dialog redesign

--- next/src/components/root-template/right-sidebar/DialogTrendingCard.tsx
+++ next/src/components/root-template/right-sidebar/DialogTrendingCard.tsx
@@ -1,7 +1,7 @@
-import { TrendingHashtagsType } from '@/context/TrendingContextProvider';
+import { TrendingHashtagType } from '@/lib/types';
 import Link from 'next/link';
 
-export default function DialogTrendingCard({ hashtag }: { hashtag: TrendingHashtagsType }) {
+export default function DialogTrendingCard({ hashtag }: { hashtag: TrendingHashtagType }) {
 
     return (
         <Link href={`http://localhost:3000/search?q=${hashtag.name}`} className='px-4 py-6 rounded flex items-center gap-8 text-14 text-secondary-text hover:bg-post-hover hover:cursor-pointer'>
