edfdc3cee20fc508eee6b735f0c1b5d242911a44
vespucchi <56511717+vespucchi@users.noreply.github.com>
2025-04-11 19:33:12 +0200

    FIX(sockets): url

--- next/src/components/root-template/left-sidebar/NewPostModal.tsx
+++ next/src/components/root-template/left-sidebar/NewPostModal.tsx
@@ -55,6 +55,7 @@ export default function NewPostModal() {
             setSelectedImagesFiles([]);
             setValue('images', []);
             reset();
+            clearErrors();
         }
     };
 
--- next/src/lib/socket.js
+++ next/src/lib/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === 'production'
-    ? undefined
+    ? 'https://tweetly.onrender.com'
     : 'http://192.168.1.155:3001';
 
 export const socket = io(URL);
